87f504c90cc8ee704d376138e726a2e86be87441
M-eerub <159754447+M-eerub@users.noreply.github.com>
2025-02-27 23:12:42 +0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,11 @@
+name = input("Enter the boy name")
+programming_language = input("Enter a programming language:(e.g: python, next js , typescript, javascript, html, css, c++)")
+mentor = input("Enter the mentor name:")
+location = input("Enter location e.g: Governor house")
+
+print("\nHere is my little story based on mad libs games!")
+print(f"One's upon a time, there was a boy name{name}.")
+print(f"{name} was very curious and always want to learn new things")
+print(f"One day, he decided to learn {programming_language} language at{location}!")
+print(f"Luckily {name} found a great mentor name as {mentor} who was expert in {programming_language}.")
+print(f"And so, his journey continues.....!")
